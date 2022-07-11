902bd32b713a814898afadb7c07359fba5dad4e6
hosamafiky <hosamafiky@gmail.com>
2022-07-11 13:18:09 +0200

    edit

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,5 +1,5 @@
 const loginRoute = '/login-route';
 const mapRoute = '/map-route';
 const otpRoute = '/otp-route';
-const googleMapsApiKey = 'AIzaSyBU6YNVxesC2-qRF2yDgCk7be8QaQz56kQ';
+const googleMapsApiKey = '';
 const googleMapsApiUrl = 'https://maps.googleapis.com/maps/api/';
